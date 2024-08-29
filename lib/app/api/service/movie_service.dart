b994564d3ec3dfe672c2c381256e33dee58cbd12
Zobayer-Mahmud <zobayermahmud00@gmail.com>
2024-08-30 05:43:30 +0600

    trending list

---
+++ lib/app/api/service/movie_service.dart
@@ -0,0 +1,7 @@
+import 'package:cinema/app/api/api_client/api_response.dart';
+import 'package:cinema/app/base/base_api_service.dart';
+import 'package:cinema/app/common/api_endpoints.dart';
+
+class MovieService extends BaseApiService {
+  getMoviesList() async {}
+}
